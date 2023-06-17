c16506746ac2767c6f1eba763367fe999fa2df0b
AurelliaPutria <lybiaaurellia@gmail.com>
2023-06-17 20:47:29 +0700

    first

---
+++ android/app/src/main/kotlin/com/example/twitter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aurel.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6420fa29b7382d795c4a';
+  static const String projectId = '6417bf334a6263c08c8d';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
